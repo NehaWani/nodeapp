0c5675b22811d4e6f65fee534aabd6b772e9ad4d
Neha Wani <neha.p.wani@gmail.com>
2015-02-16 12:27:46 -0800

    changed user.js

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/userlist', function(req, res, next) {
+router.get('/student', function(req, res, next) {
 	var db = req.db;
 	db.collection('student').find().toArray(function(err,items){
 		res.json(items);
